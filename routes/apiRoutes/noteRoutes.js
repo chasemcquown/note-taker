fdd08e8580b76c12e17a8b9d15b2a9673f2a5784
chasem <cpmcquown@gmail.com>
2021-07-21 17:22:43 -0700

    Add Heroku

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const { notes } = require('../../db/db');
+
+router.get('/db', (req, res) => {
+
+    let results = notes;
+
+    res.json(results);
+
+});
