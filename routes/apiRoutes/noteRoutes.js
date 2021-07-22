814f92757fed6906c0f90d810456a485e887b09d
chasem <cpmcquown@gmail.com>
2021-07-21 17:54:50 -0700

    Add Heroku

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const { createNewNote, validateNote } = require('../../lib/notes');
+
 const { notes } = require('../../db/db');
 
 router.get('/db', (req, res) => {
@@ -9,3 +11,17 @@ router.get('/db', (req, res) => {
     res.json(results);
 
 });
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    if (!validateNote(req.body)) {
+      res.status(400).send('Note not added.');
+    } else {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    }
+  });
+
+  module.exports = router;
